fc24000c7971627f9c436007365a11b45e7382c5
darlansaulo <darlansaulo@gmail.com>
2022-03-09 04:26:55 -0300

    Correções

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+
+    assetPrefix: isProd ? '/portifolio.curriculo.nextjs/' : '',
+
     images: {
       loader: 'akamai',
       path: '',
