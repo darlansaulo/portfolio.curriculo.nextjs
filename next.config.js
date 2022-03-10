c4186ef30db1b8449ff3fc10d238cca04fe20fa1
darlansaulo <darlansaulo@gmail.com>
2022-03-10 01:59:23 -0300

    Correções

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withImages = require("next-images");
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withImages({
   esModule: true,
+
+  assetPrefix: isProd ? '/portifolio.curriculo.nextjs/' : '',
+
   images: {
     loader: 'akamai',
     path: '',
